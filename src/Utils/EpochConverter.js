9421827d62ada5310218d1d62f67743d2bf65c5a
ct-keshav <keshav.agrawal@capillarytech.com>
2023-10-27 15:26:38 +0530

    updated formatEpochTimestamp to handle epoch in milisec

--- src/Utils/EpochConverter.js
+++ src/Utils/EpochConverter.js
@@ -1,5 +1,6 @@
 export function formatEpochTimestamp(epoch) {
-    const date = new Date(epoch);
+    const epochInMiliSec = epoch * 1000
+    const date = new Date(epochInMiliSec);
     const options = { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
     const formattedDate = date.toLocaleString('en-IN', options);
     return formattedDate;
